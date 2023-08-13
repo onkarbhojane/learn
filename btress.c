6e589f30f6dee37e66ceb96d722ae5843c562a4e
onkarbhojane <122461938+onkarbhojane@users.noreply.github.com>
2023-08-13 19:01:05 +0530

    Add files via upload

---
+++ btress.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int t,n,sum=0;
+	scanf("%d",&n);
+    int a[4];
+     for (int i=0;i<4;i++){
+        scanf("%d",&a[i]);
+     }
+    n=4;
+	for (int i=0;i<n-1;i++){
+	        int min=i;
+	        for(int j=i+1;j<n;j++){
+	            if(a[j]<a[min]){
+	                int temp=a[min];
+	               a[min]=a[j];
+	                a[j]=temp;
+	            }
+	        }
+	    }
+        for (int i=0;i<n;i++){
+            printf("%d ",a[i]);
+        }
+}
